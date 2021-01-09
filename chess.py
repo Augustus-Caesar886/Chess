724b399b09002d6f1965030f3f9da00d57eebc87
Augustus-Caesar886 <73004193+Augustus-Caesar886@users.noreply.github.com>
2021-01-08 18:59:12 -0800

    Add all pieces and begin selection logic

--- chess.py
+++ chess.py
@@ -19,6 +19,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BROWN = (165, 42, 42)
 CREAM = (255, 250, 204)
+GREEN = (0, 255, 0)
 BGCOLOR = BRIGHTBLUE
 
 KING = "KING"
@@ -49,7 +50,12 @@ class Piece:
 REPEATEDPIECES = [ROOK, KNIGHT, BISHOP]
 
 def init_board():
-    board = [[Piece(NONE, BLACK) for i in range(8)] for i in range(8)]
+    board = [[Piece(NONE, NONE) for i in range(8)] for i in range(8)]
+    for i in range(8):
+        for j in range(8):
+            board[i][j].row = i
+            board[i][j].column = j
+            
     for i in range(3):
         board[0][i] = board[0][7-i] = Piece(REPEATEDPIECES[i % 3], BLACK)
         board[0][i].row = board[0][7-i].row = 0
@@ -62,19 +68,19 @@ def init_board():
         board[7][i].column = i
         board[7][7-i].column = 7-i
     
-    board[0][4] = Piece(KING, BLACK)
+    board[0][3] = Piece(KING, BLACK)
+    board[0][3].row = 0
+    board[0][3].column = 3
+    board[7][3] = Piece(KING, WHITE)
+    board[7][3].row = 7
+    board[7][3].column = 3
+    
+    board[0][4] = Piece(QUEEN, BLACK)
     board[0][4].row = 0
     board[0][4].column = 4
-    board[7][4] = Piece(KING, WHITE)
-    board[7][4].row = 0
+    board[7][4] = Piece(QUEEN, WHITE)
+    board[7][4].row = 7
     board[7][4].column = 4
-    
-    board[0][5] = Piece(QUEEN, BLACK)
-    board[0][5].row = 0
-    board[0][5].column = 4
-    board[7][5] = Piece(QUEEN, WHITE)
-    board[7][5].row = 0
-    board[7][5].column = 4
     
     for i in range(8):
         board[1][i] = Piece(PAWN, BLACK)
@@ -92,14 +98,27 @@ board = init_board()
 
 WHITE_PAWN = pygame.image.load("wp.png")
 WHITE_ROOK = pygame.image.load("wr.png")
+WHITE_BISHOP = pygame.image.load("wb.png")
+WHITE_KNIGHT = pygame.image.load("wn.png")
+WHITE_QUEEN = pygame.image.load("wq.png")
+WHITE_KING = pygame.image.load("wk.png")
 
 BLACK_PAWN = pygame.image.load("bp.png")
 BLACK_ROOK = pygame.image.load("br.png")
+BLACK_BISHOP = pygame.image.load("bb.png")
+BLACK_KNIGHT = pygame.image.load("bn.png")
+BLACK_QUEEN = pygame.image.load("bq.png")
+BLACK_KING = pygame.image.load("bk.png")
 
 def draw_board():
     for i in range(8):
         for j in range(8):
             pygame.draw.rect(DISPLAYSURF, (CREAM if (i + j) % 2 == 0 else BROWN), (j * TILESIZE, i * TILESIZE, TILESIZE, TILESIZE))
+            if i == selectedTile[0] and selectedTile[1] == j:
+                pygame.draw.rect(DISPLAYSURF, GREEN, (j * TILESIZE, i * TILESIZE, TILESIZE, TILESIZE), 3)
+            if i == selectedPiece.row and selectedPiece.column == j:
+                pygame.draw.rect(DISPLAYSURF, GREEN, (j * TILESIZE, i * TILESIZE, TILESIZE, TILESIZE), 3)
+            
             if(board[i][j].piece_type != NONE):
                 if board[i][j].piece_type == PAWN:
                     if board[i][j].color == WHITE:
@@ -111,19 +130,61 @@ def draw_board():
                         DISPLAYSURF.blit(WHITE_ROOK, (j * TILESIZE, i * TILESIZE))
                     elif board[i][j].color == BLACK:
                         DISPLAYSURF.blit(BLACK_ROOK, (j * TILESIZE, i * TILESIZE))
+                elif board[i][j].piece_type == BISHOP:
+                    if board[i][j].color == WHITE:
+                        DISPLAYSURF.blit(WHITE_BISHOP, (j * TILESIZE, i * TILESIZE))
+                    elif board[i][j].color == BLACK:
+                        DISPLAYSURF.blit(BLACK_BISHOP, (j * TILESIZE, i * TILESIZE))
+                elif board[i][j].piece_type == KNIGHT:
+                    if board[i][j].color == WHITE:
+                        DISPLAYSURF.blit(WHITE_KNIGHT, (j * TILESIZE, i * TILESIZE))
+                    elif board[i][j].color == BLACK:
+                        DISPLAYSURF.blit(BLACK_KNIGHT, (j * TILESIZE, i * TILESIZE))
+                elif board[i][j].piece_type == QUEEN:
+                    if board[i][j].color == WHITE:
+                        DISPLAYSURF.blit(WHITE_QUEEN, (j * TILESIZE, i * TILESIZE))
+                    elif board[i][j].color == BLACK:
+                        DISPLAYSURF.blit(BLACK_QUEEN, (j * TILESIZE, i * TILESIZE))
+                elif board[i][j].piece_type == KING:
+                    if board[i][j].color == WHITE:
+                        DISPLAYSURF.blit(WHITE_KING, (j * TILESIZE, i * TILESIZE))
+                    elif board[i][j].color == BLACK:
+                        DISPLAYSURF.blit(BLACK_KING, (j * TILESIZE, i * TILESIZE))
+                
                 
     pygame.display.update()
     FPSClock.tick(FPS)
-    
+
+def get_selected(mousex, mousey):
+    for i in range(8):
+        for j in range(8):
+            checkTile = pygame.Rect((j * TILESIZE, i * TILESIZE, TILESIZE, TILESIZE))
+            if checkTile.collidepoint(mousex, mousey):
+                return board[i][j]
+
 def main():
     global DISPLAYSURF
     DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
+    global selectedTile, selectedPiece
+    selectedTile = (-1, -1)
+    selectedPiece = Piece(NONE, NONE)
+    selectedPiece.row = -1
+    selectedPiece.column = -1
     while True:
         
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                tile = get_selected(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+                if tile.piece_type != NONE:
+                    selectedPiece = tile
+                else: 
+                    selectedTile = (tile.row, tile.column)
+                
+                
+                
         draw_board()
         pygame.display.update()
         FPSClock.tick(FPS)
