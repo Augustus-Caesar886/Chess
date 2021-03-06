b62b29b01388b4e9837b29a9313af45a24e4cb09
Augustus-Caesar886 <73004193+Augustus-Caesar886@users.noreply.github.com>
2021-03-05 18:59:44 -0800

    First full release

--- chess.py
+++ chess.py
@@ -60,7 +60,7 @@ class Piece:
 REPEATEDPIECES = [ROOK, KNIGHT, BISHOP]
 
 def init_board():
-    board = [[Piece(NONE, NONE) for i in range(8)] for i in range(8)]
+    board = [[Piece(NONE, NONE) for i in range(8)] for j in range(8)]
     for i in range(8):
         for j in range(8):
             board[i][j].row = i
@@ -106,7 +106,6 @@ def init_board():
     return board
 
 
-board = init_board()
 
 WHITE_PAWN = pygame.image.load("wp.png")
 WHITE_ROOK = pygame.image.load("wr.png")
@@ -173,6 +172,7 @@ def get_selected(mousex, mousey):
             checkTile = pygame.Rect((j * TILESIZE, i * TILESIZE, TILESIZE, TILESIZE))
             if checkTile.collidepoint(mousex, mousey):
                 return board[i][j]
+    return Piece(NONE, NONE)
 
 def get_vertical_dist(piece, tile):
     return -(tile[0] - piece.row )
@@ -190,7 +190,9 @@ def check_valid_move(piece, tile):
             return True
         else:
             if(board[tile[0]][tile[1]].color != turn and board[tile[0]][tile[1]].color != NONE ):
-                if(int(get_vertical_dist(piece, tile) == 1) and abs(get_horizontal_dist(piece, tile)) == 1): #TODO: implement for black pawn
+                if(int(get_vertical_dist(piece, tile) == 1) and abs(get_horizontal_dist(piece, tile)) == 1 and piece.color == WHITE): #TODO: implement for black pawn
+                    return True
+                elif(int(get_vertical_dist(piece, tile) == -1) and abs(get_horizontal_dist(piece, tile)) == 1 and piece.color == BLACK):
                     return True
             return False
     elif piece.piece_type == ROOK:
@@ -238,7 +240,7 @@ def check_valid_move(piece, tile):
         else:
             return False
     elif piece.piece_type == QUEEN:
-        if( (get_vertical_dist(piece, tile) == get_horizontal_dist(piece, tile)) or  ((get_vertical_dist(piece, tile) != 0 and get_horizontal_dist(piece, tile)==0) or (get_vertical_dist(piece, tile) == 0 and get_horizontal_dist(piece, tile)!=0))):
+        if( (abs(get_vertical_dist(piece, tile)) == abs(get_horizontal_dist(piece, tile))) or  ((get_vertical_dist(piece, tile) != 0 and get_horizontal_dist(piece, tile)==0) or (get_vertical_dist(piece, tile) == 0 and get_horizontal_dist(piece, tile)!=0))):
             xdir = 0
             ydir = 0
             length = 0
@@ -264,6 +266,12 @@ def check_valid_move(piece, tile):
     return False
 
 def check_line_validity(piecerow, piececol, xdir, ydir, length):
+    if piecerow + (ydir*length) >= 8 or piecerow + (ydir*length) < 0:
+        return False
+    if piececol + (xdir*length) >= 8 or piececol + (xdir*length) < 0:
+        return False
+        
+    
     for i in range(length):
         if (board[piecerow + (i+1) * ydir][piececol + (i+1) * xdir].piece_type != NONE):
             
@@ -273,8 +281,135 @@ def check_line_validity(piecerow, piececol, xdir, ydir, length):
                 return False    
     return True
 
+CHECKMATE = "CHECKMATE"
+CHECK = "CHECK"
+SAFE = "SAFE"
+
+def find_king(color):
+    king_row = -1
+    king_col = -1
+    for i in range(8):
+        for j in range(8):
+            if board[i][j].piece_type == KING and board[i][j].color == color:
+                king_row = board[i][j].row
+                king_col = board[i][j].column
+    return (king_col, king_row)
+
+def invert_color(color):
+    return WHITE if color == BLACK else (BLACK if color == WHITE else NONE)
+
+def display_turn():
+    msg = "Turn: " + ("WHITE" if turn == WHITE else "BLACK")
+    font = pygame.font.Font("freesansbold.ttf", 16)
+    text = font.render(msg, True, WHITE)
+    textRect = text.get_rect()
+    textRect.topleft = (8 * TILESIZE, 0)
+    DISPLAYSURF.blit(text, textRect)
+
+def check_pawn_status():
+    for i in range(2):
+        for j in range(8):
+            if board[i*7][j].piece_type == PAWN:
+                return board[i*7][j]
+    return Piece(NONE, NONE)
+
+PAWN_UPGRADES = [WHITE_ROOK, WHITE_BISHOP, WHITE_KNIGHT, WHITE_QUEEN]
+
+def display_pawn_upgrades():
+    coords = (int(8.5 * TILESIZE), TILESIZE)
+    length = len(PAWN_UPGRADES)
+    height = TILESIZE
+    pygame.draw.rect(DISPLAYSURF, CREAM, (coords[0], coords[1], length*TILESIZE, height))
+    for i in range(length):
+        DISPLAYSURF.blit(PAWN_UPGRADES[i], (coords[0] + i * TILESIZE,coords[1]))
+
+def await_pawn_choice(pawn):
+    upgrade_coords = (int(8.5 * TILESIZE), TILESIZE)
+    choice = NONE
+    while(choice == NONE):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                mousecoords = pygame.mouse.get_pos()
+                rook_upgrade = pygame.Rect(upgrade_coords[0], upgrade_coords[1], TILESIZE, TILESIZE);
+                bishop_upgrade = pygame.Rect(upgrade_coords[0] + TILESIZE, upgrade_coords[1], TILESIZE, TILESIZE);
+                knight_upgrade = pygame.Rect(upgrade_coords[0] + (2 * TILESIZE), upgrade_coords[1], TILESIZE, TILESIZE);
+                queen_upgrade = pygame.Rect(upgrade_coords[0] + (3 * TILESIZE), upgrade_coords[1], TILESIZE, TILESIZE);
+                
+                if(rook_upgrade.collidepoint(mousecoords)):
+                    choice = ROOK
+                elif (bishop_upgrade.collidepoint(mousecoords)):
+                    choice = BISHOP
+                elif (knight_upgrade.collidepoint(mousecoords)):
+                    choice = KNIGHT
+                elif (queen_upgrade.collidepoint(mousecoords)):
+                    choice = QUEEN
+        draw_board()
+        pygame.display.update()
+        FPSClock.tick(FPS)
+    pawn.piece_type = choice
+
+def count_non_kings():
+    count = 0
+    for i in range(8):
+        for j in range(8):
+            if(board[i][j].piece_type != NONE and board[i][j].piece_type != KING):
+                count += 1
+    return count
+
+def missing_king():
+    king_coords = find_king(WHITE)
+    if(king_coords == (-1, -1)):
+        return "BLACK" #Which side wins
+    king_coords = find_king(BLACK)
+    if(king_coords == (-1, -1)):
+        return "WHITE"
+    return NONE
+
+def game_over_screen(gamestate):
+    msg = ""
+    if(gamestate != NONE):
+        msg = "Winner: " + gamestate
+    else:
+        msg = "Stalemate"
+    font = pygame.font.Font("freesansbold.ttf", 16)
+    text = font.render(msg, True, WHITE)
+    textRect = text.get_rect()
+    textRect.topleft = (9 * TILESIZE, 0)
     
+    button_msg = "Reset"
+    button_txt = font.render(button_msg, True, WHITE)
+    button_txt_rect = text.get_rect()
+    button_txt_rect.topleft = (9 * TILESIZE, TILESIZE)
     
+    pygame.draw.rect(DISPLAYSURF, BLACK, pygame.Rect((0, 0), (WINWIDTH, WINHEIGHT)))
+    
+    cont = False
+    while(not(cont)):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                mousecoords = pygame.mouse.get_pos()
+                if button_txt_rect.collidepoint(mousecoords):
+                    cont = True
+        if(not(cont)):
+            draw_board()
+            DISPLAYSURF.blit(text, textRect)
+            pygame.draw.rect(DISPLAYSURF, GREEN, button_txt_rect);
+            DISPLAYSURF.blit(button_txt, button_txt_rect)
+            FPSClock.tick(FPS)
+            pygame.display.update()
+        else:
+            pygame.draw.rect(DISPLAYSURF, BLACK, pygame.Rect((0, 0), (WINWIDTH, WINHEIGHT)))
+            draw_board()
+            FPSClock.tick(FPS)
+            pygame.display.update()
+    
+
 def main():
     global DISPLAYSURF
     DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
@@ -285,6 +420,12 @@ def main():
     selectedPiece.column = -1
     global turn
     turn = WHITE
+    gameover = False
+    winner = NONE
+    
+    global board 
+    board = init_board()
+    pygame.draw.rect(DISPLAYSURF, BLACK, pygame.Rect((0, 0), (WINWIDTH, WINHEIGHT)))
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -298,8 +439,15 @@ def main():
                 else: 
                     selectedTile = (tile.row, tile.column)
                     board[selectedTile[0]][selectedTile[1]].selected = True
-        
-        if selectedPiece.row != -1 and selectedPiece.column != -1 and selectedTile != (-1, -1):
+        if(gameover):
+            pygame.draw.rect(DISPLAYSURF, BLACK, pygame.Rect((0, 0), (WINWIDTH, WINHEIGHT)))
+            draw_board();
+            game_over_screen(winner)
+            board = init_board()
+            turn = WHITE
+            winner = NONE
+            gameover = False
+        elif selectedPiece.row != -1 and selectedPiece.column != -1 and selectedTile != (-1, -1):
             if check_valid_move(selectedPiece, selectedTile):
                 if(board[selectedTile[0]][selectedTile[1]].piece_type != NONE):
                     board[selectedTile[0]][selectedTile[1]].kill()
@@ -309,13 +457,25 @@ def main():
                     turn = BLACK
                 else:
                     turn = WHITE
+                pygame.draw.rect(DISPLAYSURF, BLACK, pygame.Rect((0, 0), (WINWIDTH, WINHEIGHT)))
+                upgrade_pawn = check_pawn_status()
+                if upgrade_pawn.piece_type != NONE:
+                    display_pawn_upgrades()
+                    await_pawn_choice(upgrade_pawn)
+                    draw_board()
+                    pygame.draw.rect(DISPLAYSURF, BLACK, pygame.Rect((0, 0), (WINWIDTH, WINHEIGHT)))
             else:
                 selectedPiece = Piece(NONE, NONE)
                 selectedPiece.row = -1
                 selectedPiece.column = -1
                 selectedTile = (-1, -1)
-                
+        elif(count_non_kings() == 0):
+            gameover = True
+        elif(missing_king() != NONE):
+            winner = missing_king()
+            gameover = True
         draw_board()
+        display_turn()
         pygame.display.update()
         FPSClock.tick(FPS)
 main()
